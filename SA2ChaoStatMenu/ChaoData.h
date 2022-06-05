31483a28dd9e384afa7eaa3bd0bf76db920ce3a6
kellsnc <kellspam@outlook.fr>
2022-06-05 15:28:00 +0200

    Use the game's d3d device instead of creating one

--- SA2ChaoStatMenu/ChaoData.h
+++ SA2ChaoStatMenu/ChaoData.h
@@ -1147,35 +1147,4 @@ enum D3D9Funcs
 	D3D9Funcs_DeletePatch = 117,
 	D3D9Funcs_CreateQuery = 118,
 };
-
-struct handle_data {
-	unsigned long process_id;
-	HWND window_handle;
-};
-
-BOOL is_main_window(HWND handle)
-{
-	return GetWindow(handle, GW_OWNER) == (HWND)0 && IsWindowVisible(handle);
-}
-
-BOOL CALLBACK enum_windows_callback(HWND handle, LPARAM lParam)
-{
-	handle_data& data = *(handle_data*)lParam;
-	unsigned long process_id = 0;
-	GetWindowThreadProcessId(handle, &process_id);
-	if (data.process_id != process_id || !is_main_window(handle))
-		return TRUE;
-	data.window_handle = handle;
-	return FALSE;
-}
-
-HWND find_main_window(unsigned long process_id)
-{
-	handle_data data;
-	data.process_id = process_id;
-	data.window_handle = 0;
-	EnumWindows(enum_windows_callback, (LPARAM)&data);
-	return data.window_handle;
-}
-
 #pragma endregion
